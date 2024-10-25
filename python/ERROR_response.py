dd52a37152e8214c4ad91cca8b912b251c9d5e7a
Sven Bingert <sven.bingert@gwdg.de>
2024-10-25 10:48:16 +0200

    new and recreated pydantic classes derived from the json schema

--- python/ERROR_response.py
+++ python/ERROR_response.py
@@ -6,44 +6,15 @@ from __future__ import annotations
 
 from typing import Literal, Optional
 
-from pydantic import BaseModel, Extra, Field, conint
-
-
-class Attributes(BaseModel):
-    class Config:
-        extra = Extra.forbid
-
-    serviceName: Optional[str] = Field(
-        None, description='the name of the DOIP service.'
-    )
-    serviceDescription: Optional[str] = Field(
-        None, description='the description of the service.'
-    )
-    ipAddress: str = Field(..., description='the IP address of the service.')
-    port: conint(ge=0, le=65535) = Field(
-        ..., description='the listening port on the IP address.'
-    )
-    protocol: str = Field(..., description='the default shall be ‘TCP’.')
-    protocolVersion: str = Field(
-        ..., description='highest version of the DOIP protocol supported.'
-    )
-    publicKey: str = Field(
-        ..., description='the public key expressed in JWK format as a default.'
-    )
+from pydantic import BaseModel, Extra, Field
 
 
 class Output(BaseModel):
     class Config:
         extra = Extra.forbid
 
-    id: str = Field(..., description='id: the identifier of the DO.')
-    type: Literal['DOIPServiceInfo'] = Field(
-        ...,
-        description='type: the DO type. Must be 0.TYPE/DO or its extension. See Types section.',
-    )
-    attributes: Attributes = Field(
-        ...,
-        description='attributes: one or more fields (key-value pairs) serialized as a JSON (sub) object.',
+    message: Optional[str] = Field(
+        None, description='A message to explain the error in more detail'
     )
 
 
@@ -56,7 +27,6 @@ class ERRORResponse(BaseModel):
         description='requestId: the identifier of the request to which this is a response. The DOIP service must include in its response the requestId provided by the client.',
     )
     status: Literal[
-        '0.DOIP/Status.001',
         '0.DOIP/Status.101',
         '0.DOIP/Status.102',
         '0.DOIP/Status.103',
